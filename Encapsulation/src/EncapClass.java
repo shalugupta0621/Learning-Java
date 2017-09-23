6b641e2261842b588b2d939abfe018c2967e4dc8
shalugupta0621 <32216768+shalugupta0621@users.noreply.github.com>
2017-09-23 14:31:51 +0530

    initial commit
    initial commit

---
+++ Encapsulation/src/EncapClass.java
@@ -0,0 +1,34 @@
+
+public class EncapClass 
+{
+	private String name;
+	private String address;
+	private int age;
+	
+	public String getName()
+	{
+		return name;
+	}
+
+	public String getAddress()
+	{
+		return address;
+	}
+	public int getAge()
+	{
+		return age;
+	}
+	public void setName(String newname)
+	{
+	name=newname;
+	}
+	public void setAddress(String newaddress)
+	{
+		address=newaddress;
+		
+	}
+	public void setAge(int newage)
+	{
+		 age=newage;
+	}
+}
---
+++ Encapsulation/src/EncapTest.java
@@ -0,0 +1,15 @@
+
+public class EncapTest {
+	
+	public static void main(String args[])
+	{
+		EncapClass e= new EncapClass();
+		e.setName("john");
+		e.setAddress("Lucknow");
+		e.setAge(20);
+		System.out.print("Name"+" "+e.getName() + " "+ "Address"+ " " +e.getAddress()+ " " + "Age" + " " + e.getAge() );
+		
+	}
+
+}
+                                                         
